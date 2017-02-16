d6a77d7c7bc7e3adbb92e2440f48052d82a507db
Lakhvir <lakhvirkumar@998.com>
2017-02-16 10:38:00 +0530

    Added return time after cart form

--- app/overrides/spree/return_authorization/product_show_return_time.rb
+++ app/overrides/spree/return_authorization/product_show_return_time.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/products/show',
   name: 'product_show_return_time',
-  insert_after: "[data-hook=description]",
+  insert_after: "[data-hook=cart_form]",
   partial: 'spree/products/return_time'
 )
