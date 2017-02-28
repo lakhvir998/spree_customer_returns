8937aba92f92ec9ed929f9331af13959dec2a69b
Lakhvir <lakhvirkumar@998.com>
2017-02-28 11:51:18 +0530

    Changed return button

--- app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
@@ -1,12 +1,11 @@
 Deface::Override.new(
   virtual_path: 'spree/orders/show',
   name: 'user_return_authorization_initailization_button',
-  insert_bottom: "#order_summary legend",
+  insert_bottom: "#cart_return form",
   text: "
         <% if @order.shipped? && @order.has_returnable_products? && @order.has_returnable_line_items? %>
-          <%= link_to(spree.new_order_return_authorization_path(@order), class: 'btn btn-primary pull-right') do %>
-            <span class='glyphicon glyphicon-send'></span>
-            <%= Spree.t(:return_products) %>
+          <%= link_to(spree.new_order_return_authorization_path(@order), class: 'btn btn-danger', 'data-toggle' => 'tooltip', 'data-original-title' => 'Return') do %>
+            <i class='fa fa-reply'></i>
           <% end %>
         <% end %>
         "
