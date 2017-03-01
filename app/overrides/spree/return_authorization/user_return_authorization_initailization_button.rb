9cba3fa47fedf6d7a2b1ecfe8ec2100c77cf119a
Lakhvir <lakhvirkumar@998.com>
2017-03-01 12:35:21 +0530

    Added return button to order detail

--- app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_initailization_button.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: 'user_return_authorization_initailization_button',
-  insert_after: "#cart_return form",
+  insert_bottom: "#cart_return",
   text: "
         <% if @order.shipped? && @order.has_returnable_products? && @order.has_returnable_line_items? %>
           <%= link_to(spree.new_order_return_authorization_path(@order), class: 'btn btn-danger', 'data-toggle' => 'tooltip', 'data-original-title' => 'Return') do %>
